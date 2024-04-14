bc2eccda16159a7daafe192c3c6ad3431bcf10fa
Nakaya <eniehack@outlook.jp>
2024-04-14 14:16:16 +0900

    fix: js, css not found

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/infop-comp-calc",
 	plugins: [
 		preact({
 			prerender: {
-				enabled: false,
+				enabled: true,
 				renderTarget: '#app',
 			},
 		}),
