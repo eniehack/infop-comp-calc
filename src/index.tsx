3b129e2c96225b083af7d4b731db001bd6bf6e6b
Nakaya <eniehack@outlook.jp>
2024-04-14 13:24:02 +0900

    feat: calculator

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,88 @@
 import { hydrate, prerender as ssr } from 'preact-iso';
 
-import preactLogo from './assets/preact.svg';
 import './style.css';
+import Subject from "../compitence.json";
+import { useCallback, useReducer, useState } from 'preact/hooks';
 
 export function App() {
+  const subject = {} as Record<string, Subject>;
+  console.log(Subject)
+  for (const comp of Subject) {
+      subject[comp["id"]] = {
+        compitences: comp["compitence"],
+        classification: comp["classification"],
+        credit: comp["credit"],
+        id: comp["id"],
+        title: comp["title"],
+      };
+  }
 	return (
-		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
+    <CompitenceContainer compitenceList={subject}/>
 	);
 }
 
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
+type Subject = {
+    compitences: number[]
+    classification: string
+    credit: number
+    id: string
+    title: string
+}
+
+function CompitenceContainer(props) {
+    const reduce = (state: Record<string, Subject>, action: {type: string, subject: Subject}) => {
+        switch (action.type) {
+            case "rm":
+                state[action.subject.id] = undefined;
+                console.log("rm");
+                console.log(state)
+                return state;
+            case "add":
+                const mergedMap = {[action.subject.id]: action.subject, ...state}
+                console.log("add");
+                console.log(mergedMap)
+                return mergedMap;
+        }
+    }
+    const [selected, dispatch] = useReducer(reduce, {} as Record<string, Subject>);
+    const createCompitence = () => {
+        return [0,0,0,0,0,0,0,0,0,0];
+    }
+    const [compitencePoint, setCompitencePoint] = useState(createCompitence());
+
+    const calculate = useCallback(() => {
+        const tmp = createCompitence();
+        for (let i = 0; i < tmp.length; i++) {
+            tmp[i] = compitencePoint[i];
+        }
+        for (const k in selected) {
+            for (let i = 0; i < tmp.length; i++) {
+                tmp[i] += props.compitenceList[k]!.compitences[i]
+            }
+        }
+        console.log(tmp);
+        setCompitencePoint(tmp);
+    }, [selected])
+
+    return (
+        <div>
+            <div>
+                {Object.values(props.compitenceList).map((subject: Subject) => (
+                    <div key={subject.id}>
+                        <label for={subject.id}>{subject.title}（{subject.id}）</label>
+                        <a href={`https://kdb.tsukuba.ac.jp/syllabi/2024/${subject.id}/jpn/0`}>KdB</a>
+                        <input type="checkbox" id={subject.id} onChange={() => dispatch({type: (typeof subject.id === "undefined" ? "rm" : "add"), subject})}/>
+                    </div>
+                ))}
+            </div>
+            <button onClick={calculate}>start</button>
+            <div>
+                {compitencePoint.map(c => (
+                    <div>{c}</div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 if (typeof window !== 'undefined') {
